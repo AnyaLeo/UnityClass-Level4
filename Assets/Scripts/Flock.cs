1d9f9b95450e9f9819dd4704add096dc4670c9e3
Anya <ania.leonova@gmail.com>
2022-07-16 10:08:57 -0700

    Lesson 4: Got the neighbors

--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -20,9 +20,28 @@ public class Flock : MonoBehaviour
     {
         for (int i = 0; i < flockAgents.Count; i++)
         {
+            List<Transform> neighbors = GetNearbyObjects(flockAgents[i]);
+            Vector3 movementDirection = CalculateAlignment(flockAgents[i], neighbors);
             flockAgents[i].MoveAgent();
+
+            // DEMO ONLY
+            // Test code
+            // The greener the agent, the more neighbors they've got
+            // This line of code is very slow for performance
+            flockAgents[i].GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, Color.green, neighbors.Count / 6f);
         }
     }
+
+    // TODO: write a function 
+    // that returns a Vector3 
+    // and takes in a FlockAgent agent and List<Transform> of its neighbors 
+
+    Vector3 CalculateAlignment(FlockAgent agent, List<Transform> neighbors)
+    {
+        Vector3 direction = new Vector3();
+
+        return direction;
+    } 
 
     void Initialize()
     {
@@ -40,5 +59,23 @@ public class Flock : MonoBehaviour
             FlockAgent newFlockAgent = newAgent.GetComponent<FlockAgent>();
             flockAgents.Add(newFlockAgent);
         }
+    }
+
+    List<Transform> GetNearbyObjects(FlockAgent agent)
+    {
+        List<Transform> neighbors = new List<Transform>();
+
+        Collider2D[] neighborColliders = Physics2D.OverlapCircleAll(agent.transform.position, 2f);
+        Collider2D agentCollider = agent.gameObject.GetComponent<Collider2D>();
+
+        for (int i = 0; i < neighborColliders.Length; i++)
+        {
+            if(neighborColliders[i] != agentCollider)
+            {
+                neighbors.Add(neighborColliders[i].transform);
+            }
+        }
+
+        return neighbors;
     }
 }
--- Assets/Scripts/FlockAgent.cs
+++ Assets/Scripts/FlockAgent.cs
@@ -4,20 +4,6 @@ using UnityEngine;
 
 public class FlockAgent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
-
     public void MoveAgent()
     {
         transform.position += transform.up * Time.deltaTime;
