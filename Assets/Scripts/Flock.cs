fc36abdac99a3a9a77ea0296b0b9df0935c85eaa
Anya <ania.leonova@gmail.com>
2022-07-28 19:48:09 -0700

    Lesson 5. Calculated alignment and avoidance behavior

--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -21,27 +21,85 @@ public class Flock : MonoBehaviour
         for (int i = 0; i < flockAgents.Count; i++)
         {
             List<Transform> neighbors = GetNearbyObjects(flockAgents[i]);
-            Vector3 movementDirection = CalculateAlignment(flockAgents[i], neighbors);
-            flockAgents[i].MoveAgent();
+            Vector3 alignmentDirection = CalculateAlignment(flockAgents[i], neighbors);
+            Vector3 cohesionDirection = CalculateCohesion(flockAgents[i], neighbors);
+
+            Vector3 moveDirection = alignmentDirection + cohesionDirection;
+
+            flockAgents[i].MoveAgent(moveDirection);
 
             // DEMO ONLY
             // Test code
             // The greener the agent, the more neighbors they've got
             // This line of code is very slow for performance
-            flockAgents[i].GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, Color.green, neighbors.Count / 6f);
+            // flockAgents[i].GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, Color.green, neighbors.Count / 6f);
         }
     }
-
-    // TODO: write a function 
-    // that returns a Vector3 
-    // and takes in a FlockAgent agent and List<Transform> of its neighbors 
 
     Vector3 CalculateAlignment(FlockAgent agent, List<Transform> neighbors)
     {
         Vector3 direction = new Vector3();
 
+        // If no neighbors, we want to return our original direction
+        if (neighbors.Count == 0)
+        {
+            return agent.transform.up;
+        }
+
+        // Get the average direction of where our neighbors are going
+        for(int i = 0; i < neighbors.Count; i++)
+        {
+            direction = direction + neighbors[i].transform.up;
+        }
+        direction = direction / neighbors.Count;
+
         return direction;
     } 
+
+    Vector3 CalculateCohesion(FlockAgent agent, List<Transform> neighbors)
+    {
+        // If we have no neighbors, return zero
+        if (neighbors.Count == 0)
+        {
+            return Vector3.zero;
+        }
+
+        // Get the average position of our neighbors
+        Vector3 direction = new Vector3();
+        for (int i = 0; i < neighbors.Count; i++)
+        {
+            direction = direction + neighbors[i].transform.position;
+        }
+        direction = direction / neighbors.Count;
+
+        // Create offset from our agent position
+        direction = direction - agent.transform.position;
+
+        return direction;
+    }
+
+    Vector3 CalculateAvoidance(FlockAgent agent, List<Transform> neighbors)
+    {
+        // If we have no neighbors, return zero
+        if (neighbors.Count == 0)
+        {
+            return Vector3.zero;
+        }
+
+        // Calculate avoidance 
+        Vector3 direction = new Vector3();
+        int numToAvoid = 0;
+        float squareAvoidanceRadius = 2f * 2f;
+
+        for(int i = 0; i < neighbors.Count; i++)
+        {
+
+        }
+
+        return direction;
+    }
+
+
 
     void Initialize()
     {
--- Assets/Scripts/FlockAgent.cs
+++ Assets/Scripts/FlockAgent.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class FlockAgent : MonoBehaviour
 {
-    public void MoveAgent()
+    public void MoveAgent(Vector3 moveDirection)
     {
+        // Rotate us in moveDirection
+        transform.up = moveDirection;
+
+        // Move us in moveDirection (because that's what out transform.up is pointing to)
         transform.position += transform.up * Time.deltaTime;
     }
 }
